561d0cf5a5cb2dd921ed54d31a5f52d863a75b7b
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2023-08-02 17:23:19 +0200

    Added first basic widget test

--- tictactoe_dart/test/widget_test.dart
+++ tictactoe_dart/test/widget_test.dart
@@ -11,20 +11,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tictactoe_dart/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('TitleWidget has title', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const TicTacToeApp());
+    await tester.pumpWidget(Directionality(
+      textDirection: TextDirection.ltr,
+      child: MediaQuery(
+        data: MediaQueryData(),
+        child: TitleWidget(),
+      ),
+    ));
+    final titleFinder = find.text("TicTacToe-Dart");
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(titleFinder, findsOneWidget);
   });
 }
