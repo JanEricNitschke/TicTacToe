9e4a15446d9cef2da2bcbc8dae5dfc40e1f2f718
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2024-01-30 21:38:59 +0100

    Added pyi file. But type hinting doesnt work yet.

--- tictactoe_c/setup.py
+++ tictactoe_c/setup.py
@@ -6,8 +6,11 @@ from setuptools import setup, Extension
 setup(
     name="tictactoe_c",
     version="0.0.1",
+    packages=["tictactoe_c"],
+    package_dir={"tictactoe_c": "tictactoe_c"},
     ext_modules=[
-        Extension("tictactoe_c", ["python_ext/tictactoe_py.c", "src/tictactoe.c"])
+        Extension("tictactoe_c", ["tictactoe_c/tictactoe_py.c", "src/tictactoe.c"])
     ],
+    package_data={"tictactoe_c": ["py.typed", "tictactoe_py.pyi"]},
     include_dirs=["include/"],
 )
---
+++ tictactoe_c/tictactoe_c/tictactoe_c.pyi
@@ -0,0 +1,5 @@
+from typing import Optional
+
+def play_game(
+    X_strength: Optional[int] = None, O_strength: Optional[int] = None
+) -> None: ...
