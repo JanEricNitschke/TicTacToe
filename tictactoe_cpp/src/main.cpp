d3cb37a884358475f27e8ff43bdd7b2deb3d1c24
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2024-04-13 10:48:03 +0200

    Enable sanitizers on linux (CI) for C++

--- tictactoe_cpp/src/main.cpp
+++ tictactoe_cpp/src/main.cpp
@@ -24,7 +24,7 @@ auto main() -> int {
     }
 
     if (isBoardFilled(board)) {
-      std::cout << "Match Draw" << std::endl;
+      std::cout << "Match drawn!" << std::endl;
       break;
     }
 
