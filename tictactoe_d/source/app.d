f9b35660395aa6c8d5df9ed6b8f0f8908c73590a
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2024-07-14 12:00:06 +0200

    Finished implementation in D

--- tictactoe_d/source/app.d
+++ tictactoe_d/source/app.d
@@ -1,6 +1,16 @@
-void main()
+void main(string[] args)
 {
     import game : play;
+    import std.getopt : getopt;
 
-    play;
+    int xStrength = 0;
+    int oStrength = 0;
+
+    getopt(
+        args,
+        "X-strength", &xStrength,
+        "O-strength", &oStrength
+    );
+
+    play(xStrength, oStrength);
 }
--- tictactoe_d/source/game.d
+++ tictactoe_d/source/game.d
@@ -1,17 +1,19 @@
 module game;
 
-immutable size_t[3][8] WIN_CONDITIONS = [
+import std.typecons : Nullable;
+
+private immutable size_t[3][8] WIN_CONDITIONS = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ];
 
-char swap(char player) @safe @nogc nothrow pure
+private char swap(const char player) @safe @nogc nothrow pure
 {
     return player == 'X' ? 'O' : 'X';
 }
 
-void show(const char[9] board) @safe
+private void show(const char[9] board) @safe
 {
     import std.stdio : writeln;
     import std.string : format;
@@ -24,7 +26,7 @@ void show(const char[9] board) @safe
     }
 }
 
-bool full(const char[9] board) @safe @nogc nothrow pure
+private bool full(const char[9] board) @safe @nogc nothrow pure
 {
     foreach (cell; board)
     {
@@ -36,7 +38,7 @@ bool full(const char[9] board) @safe @nogc nothrow pure
     return true;
 }
 
-bool is_winner(const char player, const char[9] board) @safe @nogc nothrow pure
+private bool is_winner(const char player, const char[9] board) @safe @nogc nothrow pure
 {
     foreach (condition; WIN_CONDITIONS)
     {
@@ -50,7 +52,7 @@ bool is_winner(const char player, const char[9] board) @safe @nogc nothrow pure
     return false;
 }
 
-void player_turn(char player, ref char[9] board)
+private void player_turn(const char player, ref char[9] board)
 {
     import std.stdio : writeln, stdin, stdout;
     import std.string : format, strip;
@@ -87,7 +89,183 @@ void player_turn(char player, ref char[9] board)
     }
 }
 
-void play()
+private struct EmptySpots
+{
+    private size_t[9] spots;
+    private size_t count;
+
+    static EmptySpots from_board(const char[9] board) @safe @nogc nothrow pure
+    {
+        size_t[9] empty;
+        size_t empty_count = 0;
+        foreach (i, cell; board)
+        {
+            if (cell != 'X' && cell != 'O')
+            {
+                empty[empty_count] = i;
+                ++empty_count;
+            }
+        }
+        return EmptySpots(empty, empty_count);
+    }
+
+    size_t get_random_element() @safe
+    {
+        import std.random : uniform;
+
+        auto random_index = uniform(0, count);
+        return spots[random_index];
+    }
+
+    size_t[] empties() return @safe @nogc nothrow pure
+    {
+        return spots[0 .. count];
+    }
+
+    bool empty() @safe @nogc nothrow pure
+    {
+        return count == 0;
+    }
+}
+
+private size_t ai_random_move(const char[9] board) @safe
+{
+    return EmptySpots.from_board(board).get_random_element();
+}
+
+private Nullable!size_t try_win_move(const char player, const char[9] board) @safe @nogc nothrow pure
+{
+    Nullable!size_t winning_spot;
+    foreach (condition; WIN_CONDITIONS)
+    {
+        size_t empty_count = 0;
+        size_t done_count = 0;
+        size_t empty_spot;
+        foreach (spot; condition)
+        {
+            if (board[spot] == player)
+            {
+                done_count++;
+            }
+            else if (board[spot] != player.swap)
+            {
+                ++empty_count;
+                empty_spot = spot;
+            }
+        }
+        if (empty_count == 1 && done_count == 2)
+        {
+            winning_spot = empty_spot;
+            break;
+        }
+    }
+    return winning_spot;
+}
+
+private size_t ai_win_move(const char player, const char[9] board) @safe
+{
+    auto winning_spot = try_win_move(player, board);
+    if (!winning_spot.isNull)
+    {
+        return winning_spot.get;
+    }
+    return ai_random_move(board);
+}
+
+private size_t ai_win_block_move(const char player, const char[9] board) @safe
+{
+    auto winning_move = try_win_move(player, board);
+    if (!winning_move.isNull)
+    {
+        return winning_move.get;
+    }
+    auto blocking_move = try_win_move(player.swap, board);
+    if (!blocking_move.isNull)
+    {
+        return blocking_move.get;
+    }
+    return ai_random_move(board);
+}
+
+enum Score
+{
+    WIN = 1,
+    DRAW = 0,
+    LOSE = -1
+}
+
+private struct Move
+{
+    size_t spot;
+    Score score;
+}
+
+private Move minmax(const char player, char[9] board) @safe pure
+{
+    import std.conv : to;
+
+    if (is_winner(player, board))
+    {
+        return Move(0, Score.WIN);
+    }
+    if (is_winner(player.swap, board))
+    {
+        return Move(0, Score.LOSE);
+    }
+    auto empty_spots = EmptySpots.from_board(board);
+    if (empty_spots.empty)
+    {
+        return Move(0, Score.DRAW);
+    }
+    auto best_move = Move(0, Score.LOSE);
+    foreach (spot; empty_spots.empties)
+    {
+        board[spot] = player;
+        auto score = -minmax(player.swap, board).score;
+        board[spot] = (spot + '0').to!char;
+        if (score >= best_move.score)
+        {
+            best_move = Move(spot, score);
+        }
+    }
+    return best_move;
+}
+
+private size_t ai_best_move(const char player, const char[9] board) @safe pure
+{
+    return minmax(player, board).spot;
+}
+
+private void ai_turn(const char player, ref char[9] board, const int strength)
+{
+    import std.stdio : writeln, stdout;
+    import std.string : format;
+    import core.thread : Thread, seconds;
+
+    "AI turns as player %s with strength %s".format(player, strength).writeln;
+    board.show;
+    stdout.flush;
+    size_t move;
+    switch (strength)
+    {
+    case 1:
+        move = ai_random_move(board);
+        break;
+    case 2:
+        move = ai_win_move(player, board);
+        break;
+    case 3:
+        move = ai_win_block_move(player, board);
+        break;
+    default:
+        move = ai_best_move(player, board);
+        break;
+    }
+    board[move] = player;
+    Thread.sleep(1.seconds);
+}
+
+void play(const int xStrength, const int oStrength)
 {
     import std.stdio : writeln;
     import std.string : format;
@@ -97,7 +275,19 @@ void play()
 
     while (true)
     {
-        player_turn(player, board);
+        if (player == 'X' && xStrength)
+        {
+            ai_turn(player, board, xStrength);
+        }
+        else if (player == 'O' && oStrength)
+        {
+            ai_turn(player, board, oStrength);
+        }
+        else
+        {
+            player_turn(player, board);
+
+        }
         if (is_winner(player, board))
         {
             "Player %s wins!".format(player).writeln;
@@ -113,7 +303,8 @@ void play()
     board.show;
 }
 
-@safe @nogc nothrow pure unittest
+@safe pure unittest
 {
     assert('X'.swap == 'O');
+    assert(minmax('O', ['X', '1', '2', '3', '4', '5', '6', '7', '8']) == Move(4, Score.DRAW));
 }
