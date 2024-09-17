9194e7a067ffd68655a69c6a9b07e77c5ae79360
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2024-09-17 11:20:37 +0200

    Add rust python package

---
+++ tictactoe_rust/tictactoe_rust_python/tests/run_game.py
@@ -0,0 +1,8 @@
+"""Play a tictactoe game from rust."""
+
+import tictactoe_rust_python
+
+if __name__ == "__main__":
+    tictactoe_rust_python.play_game(4, 4)
+    help(tictactoe_rust_python)
+    help(tictactoe_rust_python.play_game)
