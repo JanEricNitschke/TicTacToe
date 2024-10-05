508d50a6c63cd23df43193739c917743ee6b56ec
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2024-10-05 22:36:42 +0200

    Finished Tcl version

--- tictactoe_tcl/tictactoe.tcl
+++ tictactoe_tcl/tictactoe.tcl
@@ -1,4 +1,12 @@
 lappend auto_path "./lib"
 package require tictactoe 1.0
 
-tictactoe::play
+set X_strength 0
+set O_strength 0
+if {[llength $argv] >= 1} {
+    set X_strength [lindex $argv 0]
+}
+if {[llength $argv] >= 2} {
+    set O_strength [lindex $argv 1]
+}
+::tictactoe::play $X_strength $O_strength
